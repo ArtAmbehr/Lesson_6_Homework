1e00e7a757d64aba7b3b64a6115fc086da3d4d4b
Art <art-75@yandex.ru>
2022-11-19 14:22:22 +0300

    Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -7, 567, 89, 223-> 3
+
+Console.Write("Введите 5 чисел (положительных, отрицательных, нулей). Используйте через пробел между числами: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int positiveNumbersCount = arr.Count(x => x > 0);
+Console.WriteLine($"Количество чисел больше нуля равно: {positiveNumbersCount}");
